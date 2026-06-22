a056953cb9c4b11dcecf1bfef2caa1299368806b
Samuel King <srking@gmail.com>
2026-06-22 02:18:47 -0600

    Fix native screensaver installer artifact name

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 ./build.sh
 
 mkdir -p "$HOME/Library/Screen Savers"
-rm -rf "$HOME/Library/Screen Savers/OpenClawMarketRain.saver"
-cp -R build/OpenClawMarketRain.saver "$HOME/Library/Screen Savers/"
+rm -rf "$HOME/Library/Screen Savers/OpenClawMarketRainNative.saver"
+cp -R build/OpenClawMarketRainNative.saver "$HOME/Library/Screen Savers/"
 
-echo "Installed to: $HOME/Library/Screen Savers/OpenClawMarketRain.saver"
+echo "Installed to: $HOME/Library/Screen Savers/OpenClawMarketRainNative.saver"
